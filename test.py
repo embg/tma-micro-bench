83918ec23959cdcf858216f511d1723e5d00d637
Elliot Gorokhovsky <embg@fb.com>
2024-07-09 07:22:54 -0700

    update test

--- test.py
+++ test.py
@@ -3,13 +3,18 @@ import torch
 torch.ops.load_library("tma_kernels.so")
 
 if __name__ == "__main__":
-    m = 9 * 128
-    n = 12 * 256
-
-    src = torch.randn(m, n, device="cuda")
-    dst = torch.randn(m, n, device="cuda")
-    
-    assert not torch.allclose(src, dst)
-    torch.ops.tma_kernels.copy_2d_tma_grid_const(dst, src)
-    torch.cuda.synchronize()
-    assert torch.allclose(src, dst)
+    shapes = [
+        (256 * 9, 128 * 12),
+        (128 * 7, 256 * 13),
+        (4096, 4096),
+        (64, 64),
+    ]
+    for m,n in shapes:
+        src = torch.randn(m, n, device="cuda")
+        dst = torch.randn(m, n, device="cuda")
+        val = torch.clone(src)
+        src != dst
+        torch.ops.tma_kernels.copy_2d_tma_grid_const(dst, src)
+        torch.cuda.synchronize()
+        src == dst
+        dst == val
